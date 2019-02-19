bbc7090a02576973a4b92c1e3ec4b1891b8323b4
gbarrbrown <gbb7899@gmail.com>
2019-02-19 19:18:53 +1300

    connect weather api

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import request from 'superagent'
+
+const API_KEY = '9314d51f7275f12294c1ece77bde5ed6'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`
+    const apiRequest = request.get(url)
+
+    return {
+        type: FETCH_WEATHER,
+        payload: apiRequest
+    }
+}
